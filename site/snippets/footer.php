b3a88c1453912ab7d26ad62b8d4185d6499f7e5f
Seongki Sohn <flameware@gmail.com>
2025-09-11 23:43:09 +0900

    blog css editing for mobile

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,5 +1,10 @@
     <footer class="main-footer">
         <p>©Seongki Sohn</p>
+        <ul class="footer-menu">
+            <?php foreach ($site->children()->listed() as $item) { ?>
+            <li><a href="<?= $item->url() ?>"><?= $item->title() ?></a></li>
+            <?php } ?>    
+        </ul>
     </footer>
 </div>
 </body>
