3eac944f2570dcb8fd7f13d81150c96057648761
Seongki Sohn <flameware@gmail.com>
2026-03-04 23:22:12 +0900

    SEO fix fin

--- site/config/config.php
+++ site/config/config.php
@@ -4,4 +4,16 @@ return [
     "panel" => [
         "install" => true,
     ],
+    "routes" => [
+        [
+            "pattern" => "sitemap.xml",
+            "action" => function () {
+                $pages = site()
+                    ->index()
+                    ->filterBy("intendedTemplate", "not in", ["error"]);
+                $content = snippet("sitemap", ["pages" => $pages], true);
+                return new \Kirby\Cms\Response($content, "application/xml");
+            },
+        ],
+    ],
 ];
