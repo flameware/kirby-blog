02e5dbcdeeed84a86bbb67c7212aacdfe7551603
Seongki Sohn <flameware@gmail.com>
2026-03-16 19:46:07 +0900

    favicon added

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,6 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="index, follow" />
     <title><?= $title ?></title>
+    <link rel="icon" href="<?= url('assets/favicon.svg') ?>" type="image/svg+xml">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
 
     <style>
